cf8bbec373bde2506309c5b17373cba5191f5f2d
Manas Ranjan Swain <manas.ranjan@akqa.com>
2018-05-03 14:58:07 +0530

    update code

--- app/src/main/java/com/akqa/androiddev/android_login/LoggedinActivity.kt
+++ app/src/main/java/com/akqa/androiddev/android_login/LoggedinActivity.kt
@@ -3,15 +3,21 @@ package com.akqa.androiddev.android_login
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.content.Intent
-
+import android.widget.EditText
+import android.widget.TextView
 
 
 class LoggedinActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      //  val intent = intent
-      //  val value = intent.getStringExtra("key")
         setContentView(R.layout.activity_loggedin);
+
+        val value = getIntent().getStringExtra("key");
+        var welcome_msg = findViewById(R.id.welcome_msg) as TextView
+        welcome_msg.setText(value);
+
+
+
     }
 }
